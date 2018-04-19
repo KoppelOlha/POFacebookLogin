6cac12788e203818e3cb30e4e16dcc9338159401
HOME <olga81koppel@gmail.com>
2018-04-19 20:33:25 +0300

    New version

--- src/main/java/MainPage.java
+++ src/main/java/MainPage.java
@@ -1,16 +1,15 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 
 
-public class MainPage {
+public class MainPage extends BrowserManager {
     private static final String LOGIN = "";
     private static final String PASSWORD = "";
 
-    public void loginFacebook(WebDriver browser) {
-        browser.get("https://www.facebook.com");
-        browser.findElement(By.cssSelector("[type~=email]")).sendKeys(LOGIN);
-        browser.findElement(By.cssSelector("#pass")).sendKeys(PASSWORD);
-        browser.findElement(By.xpath(".//label[@id=\"loginbutton\"]")).click();
+    public FeedPage loginFacebook() {
+        BrowserManager.browser.findElement(By.cssSelector("[type~=email]")).sendKeys(LOGIN);
+        BrowserManager.browser.findElement(By.cssSelector("#pass")).sendKeys(PASSWORD);
+        BrowserManager.browser.findElement(By.xpath(".//label[@id='loginbutton']")).click();
+        return new FeedPage();
     }
 
 }
